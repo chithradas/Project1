072af5924300f808f92a10537227fa39320270d5
mallikarjuna gonuguntla <gmallikharjuna01@gmail.com>
2023-04-16 18:03:45 +0100

    added program files of python

--- category.py
+++ category.py
@@ -1,5 +1,6 @@
+import sqlite3
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
 
 
 class categoryClass:
@@ -15,14 +16,20 @@ class categoryClass:
 
         self.var_emp_id = StringVar()
 
+
         # ---frame
         lbl_title = Label(self.root, text="Manage Product Category", font=("goudy old style", 25, "bold"), bg="blue",fg="white",bd=3).pack(side=TOP,fill=X)
 
-        lbl_name = Label(self.root,text="Enter Category Name",font=("goudy old style", 25, "bold"),fg="black").place(x= 120,y=120)
-        txt_name = Entry(self.root, font=("goudy old style", 25, "bold"),bg="lightyellow",fg="black").place(x=650,y=120)
+        lbl_cat_id = Label(self.root, text="Enter Category id", font=("goudy old style", 25, "bold"), fg="black").place(
+            x=120, y=80)
+        txt_cat_id = Entry(self.root,textvariable=self.var_cat_id, font=("goudy old style", 25, "bold"), bg="lightyellow", fg="black").place(x=600,
+                                                                                                              y=80)
 
-        btn_add = Button(self.root,text="Add",font=("goudy old style", 25, "bold"), bg="darkgreen", fg="white").place(x=650,y=320,width=120)
-        btn_delete = Button(self.root, text="Delete", font=("goudy old style", 25, "bold"), bg="darkred", fg="white").place(x=850, y=320, width=120)
+        lbl_name = Label(self.root,text="Enter Category Name",font=("goudy old style", 25, "bold"),fg="black").place(x= 120,y=150)
+        txt_name = Entry(self.root,textvariable=self.var_name,  font=("goudy old style", 25, "bold"),bg="lightyellow",fg="black").place(x=600,y=150)
+
+        btn_add = Button(self.root,text="Add",font=("goudy old style", 25, "bold"),command= self.add, bg="darkgreen", fg="white").place(x=650,y=320,width=120)
+        btn_delete = Button(self.root, text="Delete", font=("goudy old style", 25, "bold"),command= self.delete, bg="darkred", fg="white").place(x=850, y=320, width=120)
 
         # ------------------employee frame
 
@@ -32,24 +39,101 @@ class categoryClass:
         scrolly = Scrollbar(emp_frame, orient=VERTICAL)
         scrollx = Scrollbar(emp_frame, orient=HORIZONTAL)
 
-        self.EmployeeTable = ttk.Treeview(emp_frame, columns=("cid", "name"),yscrollcommand=scrolly.set, xscrollcommand=scrollx.set)
+        self.CategoryTable = ttk.Treeview(emp_frame, columns=("cid", "name"),yscrollcommand=scrolly.set, xscrollcommand=scrollx.set)
         scrollx.pack(side=BOTTOM, fill=X)
         scrolly.pack(side=RIGHT, fill=Y)
-        scrollx.config(command=self.EmployeeTable.xview)
-        scrolly.config(command=self.EmployeeTable.yview)
-        self.EmployeeTable.heading("cid", text="Cat Id")
-        self.EmployeeTable.heading("name", text="Name")
+        scrollx.config(command=self.CategoryTable.xview)
+        scrolly.config(command=self.CategoryTable.yview)
+        self.CategoryTable.heading("cid", text="Cat Id")
+        self.CategoryTable.heading("name", text="Name")
 
 
 
-        self.EmployeeTable.column("cid", width=50)
-        self.EmployeeTable.column("name", width=50)
+        self.CategoryTable.column("cid", width=50)
+        self.CategoryTable.column("name", width=50)
 
 
 
-        self.EmployeeTable["show"] = "headings"
-        self.EmployeeTable.pack(fill=BOTH, expand=1)
+        self.CategoryTable["show"] = "headings"
+        self.CategoryTable.pack(fill=BOTH, expand=1)
+        self.CategoryTable.bind("<ButtonRelease-1>", self.get_data)
+        self.show()
 
+    def add(self):
+        con = sqlite3.connect(database=r"ims.db")
+        cur = con.cursor()
+        try:
+            if self.var_cat_id.get() == "":
+                messagebox.showerror("Error", "Id Must be required", parent=self.root)
+            else:
+                cur.execute("Select * from category where  catId=?", (self.var_cat_id.get(),))
+                row = cur.fetchone()
+                if row != None:
+                    messagebox.showerror("Error", "This Id already assigned, try diffrent", parent=self.root)
+                else:
+                    cur.execute(
+                        "INSERT INTO category( catId, name) VALUES (?, ?)",
+                        (
+                            self.var_cat_id.get(),
+                            self.var_name.get(),
+
+                        ))
+                    con.commit()
+                    messagebox.showinfo("Success", "category added succesfully", parent=self.root)
+
+                    self.show()
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to : {str(ex)},parent=self.root")
+
+    def get_data(self, ev):
+        f = self.CategoryTable.focus()
+        content = (self.CategoryTable.item(f))
+        row = content['values']
+
+        self.var_cat_id.set(row[0]),
+        self.var_name.set(row[1]),
+
+
+    def show(self):
+        con=sqlite3.connect(database='ims.db')
+        cur=con.cursor()
+        try:
+            cur.execute("select * from  category")
+            rows = cur.fetchall()
+            self.CategoryTable.delete(*self.CategoryTable.get_children())
+            for row in rows:
+                self.CategoryTable.insert('',END,values=row)
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to : {str(ex)}",parent=self.root)
+
+    def delete(self):
+        con = sqlite3.connect(database=r"ims.db")
+        cur = con.cursor()
+        try:
+            if self.var_cat_id.get() == "":
+                messagebox.showerror("Error", "ID Must be required", parent=self.root)
+            else:
+                cur.execute("Select * from category where catId=?", (self.var_cat_id.get(),))
+                row = cur.fetchone()
+                if row == None:
+                    messagebox.showerror("Error", "Invalid category ID", parent=self.root)
+                else:
+                    op =messagebox.askyesno("Confirm","Do you real want to delete?")
+                    if op == True:
+
+                        cur.execute("delete from category where catId=?",(self.var_cat_id.get(),))
+                        con.commit()
+                        messagebox.showinfo("Delete","Employee deleted successfully",parent=self.root)
+
+                        self.clear()
+        except Exception as ex:
+            messagebox.showerror("Error", f"Error due to : {str(ex)}",parent=self.root)
+
+    def clear(self):
+        self.var_cat_id.set(""),
+        self.var_name.set(""),
+
+        self.show()
 
 if __name__=="__main__":
     root = Tk()
--- suppliers.py
+++ suppliers.py
@@ -17,25 +17,19 @@ class suppliersClass:
         self.var_searchby = StringVar()
         self.var_searchtxt = StringVar()
 
-        self.var_emp_id = StringVar()
-        self.var_gender = StringVar()
+        self.var_sup_invoice = StringVar()
         self.var_contact = StringVar()
         self.var_name = StringVar()
-        self.var_dob = StringVar()
-        self.var_doj = StringVar()
-        self.var_email = StringVar()
-        self.var_pass = StringVar()
-        self.var_utype = StringVar()
-        self.var_salary = StringVar()
-
-        # ------------search frame--------------
-        SearchFrame = LabelFrame(self.root, text="Invoice Number", bg="white")
-        SearchFrame.place(x=250, y=20, width=600, height=70)
+        self.var_desc= StringVar()
 
 
-        txt_search = Entry(SearchFrame, bg='lightyellow').place(x=200, y=10)
-        btn_srch = Button(SearchFrame, text='Search', bg='darkgreen', fg='white', cursor='hand2').place(x=400, y=10,
-                                                                                                        width=150)
+        self.var_name = StringVar()
+
+
+
+
+
+
 
         # ---frame
         self.lbl_clock = Label(self.root, text="Manage suppliers Details", font=("times new roman", 15, "bold"), bg="blue",
@@ -51,10 +45,10 @@ class suppliersClass:
         lbl_contact = Label(self.root, text="Contact", font=("times new roman", 11, "bold"), bg="white",
                             fg="black").place(x=750, y=150)
 
-        txt_inv_no = Entry(self.root, textvariable=self.var_emp_id, bg="lightyellow", fg="black").place(x=150, y=150,
+        txt_inv_no = Entry(self.root, textvariable=self.var_sup_invoice, bg="lightyellow", fg="black").place(x=150, y=150,
                                                                                                        width=180)
 
-        txt_supply = Entry(self.root, textvariable=self.var_gender, bg="white", fg="black").place(x=500, y=150,
+        txt_supply = Entry(self.root, textvariable=self.var_name, bg="lightyellow", fg="black").place(x=500, y=150,
                                                                                                   width=180)
 
 
@@ -65,20 +59,20 @@ class suppliersClass:
         lbl_desc = Label(self.root, text="Description", font=("times new roman", 11, "bold"), bg="white",
                             fg="black").place(x=40, y=250)
 
-        txt_inv_no = Entry(self.root, textvariable=self.var_emp_id, bg="lightyellow", fg="black").place(x=150, y=250,height=75,
+        txt_desc = Entry(self.root, textvariable=self.var_desc, bg="lightyellow", fg="black").place(x=150, y=250,height=75,
                                                                                                         width=400)
 
         # -------------------------button
         btn_add = Button(self.root, text='Save',command= self.add, bg='darkgreen', fg='white', cursor='hand2').place(x=550, y=305,
                                                                                                    width=110)
-        btn_update = Button(self.root, text='Update', bg='grey', fg='white', cursor='hand2').place(x=700, y=305,
+        btn_update = Button(self.root, text='Update',command= self.update, bg='grey', fg='white', cursor='hand2').place(x=700, y=305,
                                                                                                    width=110)
-        btn_delete = Button(self.root, text='Delete', bg='darkred', fg='white', cursor='hand2').place(x=850, y=305,
+        btn_delete = Button(self.root, text='Delete',command= self.delete, bg='darkred', fg='white', cursor='hand2').place(x=850, y=305,
                                                                                                       width=110)
-        btn_clear = Button(self.root, text='clear', bg='darkblue', fg='white', cursor='hand2').place(x=1000, y=305,
+        btn_clear = Button(self.root, text='clear',command= self.clear, bg='darkblue', fg='white', cursor='hand2').place(x=1000, y=305,
                                                                                                      width=110)
 
-        # ------------------employee frame
+        # ------------------suppliers frame
 
         emp_frame = Frame(self.root, bd=3, relief=RIDGE)
         emp_frame.place(x=0, y=350, relwidth=1, height=350)
@@ -86,45 +80,47 @@ class suppliersClass:
         scrolly = Scrollbar(emp_frame, orient=VERTICAL)
         scrollx = Scrollbar(emp_frame, orient=HORIZONTAL)
 
-        self.EmployeeTable = ttk.Treeview(emp_frame, columns=("sid", "invno", "name", "contact"),yscrollcommand=scrolly.set, xscrollcommand=scrollx.set)
+        self.SupplierTable = ttk.Treeview(emp_frame, columns=("sid", "invno", "name", "contact"),yscrollcommand=scrolly.set, xscrollcommand=scrollx.set)
         scrollx.pack(side=BOTTOM, fill=X)
         scrolly.pack(side=RIGHT, fill=Y)
-        scrollx.config(command=self.EmployeeTable.xview)
-        scrolly.config(command=self.EmployeeTable.yview)
-        self.EmployeeTable.heading("sid", text="Supp Id")
-        self.EmployeeTable.heading("invno", text="Inv No.")
-        self.EmployeeTable.heading("name", text="Name")
-        self.EmployeeTable.heading("contact", text="Contact")
+        scrollx.config(command=self.SupplierTable.xview)
+        scrolly.config(command=self.SupplierTable.yview)
+        self.SupplierTable.heading("sid", text="Invoices")
+        self.SupplierTable.heading("invno", text="Contact")
+        self.SupplierTable.heading("name", text="Name")
+        self.SupplierTable.heading("contact", text="Description")
 
-        self.EmployeeTable.column("sid", width="50")
-        self.EmployeeTable.column("invno", width="50")
-        self.EmployeeTable.column("name", width="50")
-        self.EmployeeTable.column("contact", width="50")
+        self.SupplierTable.column("sid", width="50")
+        self.SupplierTable.column("invno", width="50")
+        self.SupplierTable.column("name", width="50")
+        self.SupplierTable.column("contact", width="50")
 
 
 
-        self.EmployeeTable["show"] = "headings"
-        self.EmployeeTable.pack(fill=BOTH, expand=1)
+        self.SupplierTable["show"] = "headings"
+        self.SupplierTable.pack(fill=BOTH, expand=1)
+        self.SupplierTable.bind("<ButtonRelease-1>", self.get_data)
+        self.show()
 
     def add(self):
         con = sqlite3.connect(database=r"ims.db")
         cur = con.cursor()
         try:
-            if self.var_emp_id.get() == "":
-                messagebox.showerror("Error", "Employee ID Must be required", parent=self.root)
+            if self.var_sup_invoice.get() == "":
+                messagebox.showerror("Error", "Invoice Must be required", parent=self.root)
             else:
-                cur.execute("Select * from employee where eid=?", (self.var_emp_id.get(),))
+                cur.execute("Select * from supplier where invoice=?", (self.var_sup_invoice.get(),))
                 row = cur.fetchone()
                 if row != None:
-                    messagebox.showerror("Error", "This employee ID already assigned, try diffrent", parent=self.root)
+                    messagebox.showerror("Error", "This invoice already assigned, try diffrent", parent=self.root)
                 else:
                     cur.execute(
-                        "INSERT INTO employee (sid, invno, name, contact) VALUES (?, ?, ?, ?)",
+                        "INSERT INTO supplier( invoice, name, contact,desc) VALUES (?, ?, ?, ?)",
                         (
-                            self.var_emp_id.get(),
+                            self.var_sup_invoice.get(),
                             self.var_name.get(),
-                            self.var_email.get(),
-                            self.var_gender.get()
+                            self.var_contact.get(),
+                            self.var_desc.get()
                         ))
                     con.commit()
                     messagebox.showinfo("Success", "Employee added succesfully", parent=self.root)
@@ -132,6 +128,87 @@ class suppliersClass:
                     self.show()
         except Exception as ex:
             messagebox.showerror("Error",f"Error due to : {str(ex)},parent=self.root")
+
+    def get_data(self, ev):
+        f = self.SupplierTable.focus()
+        content = (self.SupplierTable.item(f))
+        row = content['values']
+
+        self.var_sup_invoice.set(row[0]),
+        self.var_name.set(row[1]),
+        self.var_contact.set(row[2]),
+        self.var_desc.set(row[3]),
+
+
+
+    def show(self):
+        con=sqlite3.connect(database='ims.db')
+        cur=con.cursor()
+        try:
+            cur.execute("select * from supplier")
+            rows = cur.fetchall()
+            self.SupplierTable.delete(*self.SupplierTable.get_children())
+            for row in rows:
+                self.SupplierTable.insert('',END,values=row)
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to : {str(ex)}",parent=self.root)
+
+    def update(self):
+        con = sqlite3.connect(database=r"ims.db")
+        cur = con.cursor()
+        try:
+            if self.var_sup_invoice.get() == "":
+                messagebox.showerror("Error", "Invoice must be required", parent=self.root)
+            else:
+                cur.execute("SELECT * FROM supplier WHERE invoice=?", (self.var_sup_invoice.get(),))
+                row = cur.fetchone()
+                if row is None:
+                    messagebox.showerror("Error", "Invalid invoice number", parent=self.root)
+                else:
+                    cur.execute(
+                        "UPDATE supplier SET name=?, contact=?, desc=? WHERE invoice=?",
+                        (
+                            self.var_name.get(),
+                            self.var_contact.get(),
+                            self.var_desc.get(),
+                            self.var_sup_invoice.get()
+                        )
+                    )
+                    con.commit()
+                    messagebox.showinfo("Success", "Supplier updated successfully", parent=self.root)
+                    self.show()
+        except Exception as ex:
+            messagebox.showerror("Error", f"Error due to: {str(ex)}", parent=self.root)
+
+    def delete(self):
+        con = sqlite3.connect(database=r"ims.db")
+        cur = con.cursor()
+        try:
+            if self.var_sup_invoice.get() == "":
+                messagebox.showerror("Error", "Invoice Must be required", parent=self.root)
+            else:
+                cur.execute("Select * from supplier where invoice=?", (self.var_sup_invoice.get(),))
+                row = cur.fetchone()
+                if row == None:
+                    messagebox.showerror("Error", "Invalid Invoice", parent=self.root)
+                else:
+                    op =messagebox.askyesno("Confirm","Do you really want to delete?")
+                    if op == True:
+
+                        cur.execute("delete from supplier where invoice=?",(self.var_sup_invoice.get(),))
+                        con.commit()
+                        messagebox.showinfo("Delete","Supplier deleted successfully",parent=self.root)
+
+                        self.clear()
+        except Exception as ex:
+            messagebox.showerror("Error", f"Error due to : {str(ex)}",parent=self.root)
+
+    def clear(self):
+        self.var_sup_invoice.set(""),
+        self.var_name.set(""),
+        self.var_contact.set(""),
+        self.var_desc.set("")
+
 
 
 if __name__=="__main__":
